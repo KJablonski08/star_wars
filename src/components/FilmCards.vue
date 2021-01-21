c47c0fa16c3e15c22627cc8a581f9ef69e6a7d44
KJablonski08 <kjablonski08@gmail.com>
2021-01-21 14:28:49 -0500

    films working with select form as expected

--- src/components/FilmCards.vue
+++ src/components/FilmCards.vue
@@ -3,28 +3,17 @@
     <!-- SELECT FORM --> 
         <b-form-select v-model="selected" :options="options" size="sm" class="mt-3 w-50 p-3 mb-1 bg-secondary text-light" v-on:change="updateResults" ></b-form-select>
         <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
-        <!-- SELECT FORM --> 
-        <div class="accordion" role="tablist">
+    <!-- ACCORDION --> 
+        <div class="accordion" role="tablist" v-for="(item, index) in results" :key="index">
             <b-card no-body class="mb-1">
                 <b-card-header header-tag="header" class="p-1" role="tab">
-                    <b-button block v-b-toggle.accordion-1 variant="info">Accordion 1</b-button>
+                    <b-button block v-b-toggle="'collapse' + index" variant="dark">{{item.title}}</b-button>
                 </b-card-header>
-                <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
+                <b-collapse :id="'collapse' + index" accordion="my-accordion" role="tabpanel">
                     <b-card-body>
-                        <b-card-text>{{ this.results }}</b-card-text>
+                        <b-card-text v-for="(key, value) in item" :key="value"><strong>{{value.toUpperCase()}}</strong> : {{key}}</b-card-text>
                     </b-card-body>
                 </b-collapse>
-            </b-card>
-
-            <b-card no-body class="mb-1">
-            <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block v-b-toggle.accordion-2 variant="info">Accordion 2</b-button>
-            </b-card-header>
-            <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
-                <b-card-body>
-                <b-card-text>{{ text }}</b-card-text>
-                </b-card-body>
-            </b-collapse>
             </b-card>
         </div>
 	</div>
