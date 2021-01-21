2da1ac6ad25bd83bdc2b3871a3585bf3978b8fe2
KJablonski08 <kjablonski08@gmail.com>
2021-01-21 16:06:40 -0500

    updated to secure url for deployment

--- src/components/FilmCards.vue
+++ src/components/FilmCards.vue
@@ -1,10 +1,12 @@
 <template>
 	<div>
     <!-- SELECT FORM --> 
-        <b-form-select v-model="selected" :options="options" size="sm" class="mt-3 w-50 p-3 mb-1 bg-secondary text-light" v-on:change="updateResults" ></b-form-select>
-        <div class="mt-3" v-if="selected">Selected: <strong>{{ selected.toUpperCase() }}</strong></div>
+        <h4 class="mt-3">Welcome, Select the Star Wars data would you like to know more about...</h4>
+        <b-form-select v-model="selected" :options="options" size="sm" class="mt-3 w-50 p-3 mb-1 bg-secondary text-light" v-on:change="updateResults">
+        </b-form-select>
+        <div class="mt-3 mb-3" v-if="selected">Selected: <strong>{{ selected.toUpperCase() }}</strong></div>
         <div class="d-flex justify-content-center">
-            <b-form-input list="my-list-id"  v-show="results && selected !== 'films'" class="w-50" v-on:input="filterList"></b-form-input>
+            <b-form-input list="my-list-id"  v-show="results && selected !== 'films'" class="w-50 mb-3" v-on:input="filterList" placeholder="Filter here..."></b-form-input>
 
 			<datalist id="my-list-id" v-if="selected === 'films'">
 				<option v-for="(item, index) in results" :key="index">{{
@@ -52,7 +54,6 @@
             selected: null,
             search: null,
             options: [
-                { value: null, text: 'Please select an option' },
                 { value: 'films', text: 'films' },
                 { value: 'planets', text: 'planets' },
                 { value: 'starships', text: 'starships' },
@@ -65,7 +66,7 @@
     methods: {
        updateResults() {
            axios
-			.get("http://swapi.dev/api/" + this.selected + "/")
+			.get("https://swapi.dev/api/" + this.selected + "/")
 			.then((response) => (this.results = response.data.results))
        },
        filterList() {
